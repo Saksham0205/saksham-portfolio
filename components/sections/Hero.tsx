dfd8ff68c99142d3380cf305cc9bf9cf9f3b88a4
Saksham0205 <saksham252003@gmail.com>
2026-04-24 11:39:11 +0530

    update resume download link in Hero section to new URL

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -81,7 +81,7 @@ export function Hero({ heroRef }: HeroProps) {
                 <a href="#projects">View Projects</a>
               </Button>
               <Button asChild variant="outline" size="lg" className="w-full sm:w-auto bg-transparent border-[#00f5ff]/30 text-[#00f5ff] hover:bg-[#00f5ff]/10 hover:border-[#00f5ff]/60">
-                <a href="https://drive.google.com/uc?export=download&id=1Dzwnw-8Ro424AoNs7V61_ivQPw1-V887" download="Saksham_Resume.pdf">
+                <a href="https://drive.google.com/uc?export=download&id=1lwalw48e_vPbcIYh1iGyvzrhXJyFaxri" download="Saksham_Resume.pdf">
                   Download Resume
                 </a>
               </Button>
